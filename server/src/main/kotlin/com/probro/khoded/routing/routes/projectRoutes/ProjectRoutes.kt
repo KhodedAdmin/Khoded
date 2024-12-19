17ab92afb843b5a172a49256a9df1a11a8b7b4f0
Joshua Dronyi <joshuadronyi@gmail.com>
2024-12-18 20:44:30 -0500

    WIP: Updated to main

--- server/src/main/kotlin/com/probro/khoded/routing/routes/projectRoutes/ProjectRoutes.kt
+++ server/src/main/kotlin/com/probro/khoded/routing/routes/projectRoutes/ProjectRoutes.kt
@@ -23,32 +23,11 @@ class Projects {
 
 fun Application.projectRouting() {
     routing {
-        authenticate(AuthTypes.OAUTH.name) {
-            get<Projects> {
-                val cookie = call.sessions.get(Cookies.USER_COOKIE.value) as UserCookie
-                cookie.userID?.let { id ->
-                    val projects = ProjectRepository.getProjectsForUserID(id)
-                    call.respond(
-                        status = HttpStatusCode.OK,
-                        message = json.encodeToString(projects)
-                    )
-                } ?: call.respond(
-                    HttpStatusCode.Unauthorized,
-                    "You need to log in before seeing project details"
-                )
-            }
-            get<Projects.CreateNewProject> {
-                //TODO: Create a new project associated with this user.
-            }
-            get<Projects.ID> {
-                //TODO: RETURN THE PROJECT WITH THE SPECIFIC ID
-            }
-            post<Projects.ID.Edit> {
-                //TODO: Update the project with this Id according to incoming data.s
-            }
-            post<Projects.ID.Delete> {
-                //TODO: Delete the project with this ID from the Database.
-            }
+        authenticate(AuthTypes.BASE_AUTH.name) {
+            createNewProject()
+            getProjects()
+            editProject()
+            deleteProject()
         }
     }
 }
